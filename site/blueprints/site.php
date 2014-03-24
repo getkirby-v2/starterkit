12789eef6505f96954b8d7b8e90d387bcffb7082
Bastian Allgeier <bastian.allgeier@gmail.com>
2014-03-24 19:51:42 +0100

    added the core, panel, license and readme

--- site/blueprints/site.php
+++ site/blueprints/site.php
@@ -6,9 +6,15 @@ fields:
   title: 
     label: Title
     type:  text
+  author: 
+    label: Author
+    type:  text
   keywords: 
     label: Keywords
     type:  tags
   description: 
     label: Description
     type:  textarea
+  copyright: 
+    label: Copyright
+    type:  text
