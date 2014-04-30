5e0d02927f3171d9cbc06bce24d6df2b7445fbe1
Mészáros Róbert <alt@meszarosrob.com>
2014-04-30 19:24:54 +0200

    dispaly only visible projects
    mimic the main nav

--- site/snippets/projects.php
+++ site/snippets/projects.php
@@ -1,6 +1,6 @@
 <h2>Latest projects</h2>
 <ul class="teaser cf">
-  <?php foreach(page('projects')->children()->limit(3) as $project): ?>
+  <?php foreach(page('projects')->children()->visible()->limit(3) as $project): ?>
   <li>
     <h3><a href="<?php echo $project->url() ?>"><?php echo html($project->title()) ?></a></h3>
     <p><?php echo excerpt($project->text(), 80) ?> <a href="<?php echo $project->url() ?>">read&nbsp;more &rsaquo;</a></p>
