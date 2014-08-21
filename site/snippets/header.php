303705c618cb3a1d4b05ed74033d63f186741e77
sashtown <git@sashtown.de>
2014-08-21 15:14:28 +0200

    re-added the viewport metatag

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -3,6 +3,7 @@
 <head>
 
   <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
   <title><?php echo html($site->title()) ?> | <?php echo html($page->title()) ?></title>
   <meta name="description" content="<?php echo html($site->description()) ?>" />
