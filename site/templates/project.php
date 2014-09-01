78b7467471c0274ad86a6fff6551b30ef869bdd9
Bastian Allgeier <mail@bastianallgeier.com>
2014-09-01 13:22:19 +0200

    Fix #16

--- site/templates/project.php
+++ site/templates/project.php
@@ -12,7 +12,7 @@
     <div class="text">
       <?php echo kirbytext($page->text()) ?>
 
-      <?php foreach($page->images() as $image): ?>
+      <?php foreach($page->images()->sortBy('sort', 'asc') as $image): ?>
       <figure>
         <img src="<?php echo $image->url() ?>" alt="<?php echo html($page->title()) ?>">
       </figure>
