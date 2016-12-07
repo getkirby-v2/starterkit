67b756620f765241b0e2cd93edda4819afa1181c
Fabian Michael <hallo@fabianmichael.de>
2016-12-07 15:57:47 +0100

    Remove “self-closing” slash for meta tags.

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -2,8 +2,8 @@
 <html lang="<?= site()->language() ? site()->language()->code() : 'en' ?>">
 <head>
 
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
   <title><?= $site->title()->html() ?> | <?= $page->title()->html() ?></title>
   <meta name="description" content="<?= $site->description()->html() ?>">
