006d189fb79a97685ff718159af5c9d389bbb389
Bastian Allgeier <mail@bastianallgeier.com>
2017-06-22 10:11:07 +0200

    Update Kirby to 2.5.1

--- panel/app/src/panel.php
+++ panel/app/src/panel.php
@@ -35,13 +35,13 @@ use Kirby\Panel\Models\Page\Blueprint as PageBlueprint;
 
 class Panel {
 
-  static public $version = '2.5.0';
+  static public $version = '2.5.1';
 
   // minimal requirements
   static public $requires = array(
     'php'     => '5.4.0',
-    'toolkit' => '2.4.1',
-    'kirby'   => '2.4.1'
+    'toolkit' => '2.5.1',
+    'kirby'   => '2.5.1'
   );
 
   static public $instance;
